ae801606525173244fff76fb8a79d4bae223de10
mohamad-tarrab-pro <mohamad.tarrab.pro@gmail.com>
2026-02-16 15:14:47 +0000

    ajout de nouveau ficiers de test python et test unitaires

---
+++ exercices/mon_premier_script.py
@@ -0,0 +1,17 @@
+message = "C'est le premier scripte"
+print (message)
+
+prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+more_than_seven = 0
+for prenom in prenoms:
+    if len(prenom) > 7:
+        more_than_seven += 1
+        print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
+    else:
+        print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
+print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(more_than_seven))
+
+def saluer(nom: str) -> str:
+    return "Bonjour " + nom
+
+print(saluer("Alice"))  # Affiche : Bonjour Alice

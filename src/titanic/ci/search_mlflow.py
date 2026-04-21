b4391bdf29f30c1062d9ec9da42c1c3c72752d1b
mohamad-tarrab-pro <mohamad.tarrab.pro@gmail.com>
2026-04-21 16:29:44 +0000

    fix: search mlflow

--- src/titanic/ci/search_mlflow.py
+++ src/titanic/ci/search_mlflow.py
@@ -9,6 +9,7 @@ def get_last_model_uri(experiment_name: str) -> str:
     logging.warning(f"experiment_name: {experiment_name}")
     current_experiment = dict(mlflow.get_experiment_by_name(experiment_name))
     experiment_id = current_experiment['experiment_id']
+    
     runs: list[Run] = mlflow.search_runs(
         [experiment_id],
         filter_string="attributes.status = 'FINISHED'",
@@ -16,11 +17,14 @@ def get_last_model_uri(experiment_name: str) -> str:
         order_by=["attributes.end_time DESC"],
         output_format="list"
     )
-    run = mlflow.get_run(runs[0].info.run_id)
-    logging.warning(f"Found model id: {run.outputs.model_outputs[0].model_id}")
-    model_uri = f"models:/{run.outputs.model_outputs[0].model_id}"
-    logging.warning(f"Returning: {model_uri}")
-    return model_uri
+    
+    # On extrait directement le run_id de la liste des runs trouvés
+    run_id = runs[0].info.run_id
+    
+    logging.warning(f"Returning run_id: {run_id}")
+    
+    # C'est ce retour qui sera capturé par "RUN_ID=$(...)" dans la GitHub Action
+    return run_id
 
 
 if __name__ == "__main__":
